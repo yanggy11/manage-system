9bcacca639913de2fed16223dcebc60b807cc1c5
yanggy <yangguiyun250725@163.com>
2018-08-22 13:12:32 +0800

    升级vue

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -6,6 +6,8 @@ var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
+const { VueLoaderPlugin } = require('vue-loader')
+
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
   baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
@@ -30,6 +32,7 @@ module.exports = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true
     }),
-    new FriendlyErrorsPlugin()
+    new FriendlyErrorsPlugin(),
+    new VueLoaderPlugin()
   ]
 })
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App';
 import router from './router';
 import axios from 'axios';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-default/index.css';    // 默认主题
+import 'element-ui/lib/theme-chalk/index.css';    // 默认主题
 //import '../static/css/theme-green/index.css';       // 浅绿色主题
 import "babel-polyfill";
 import VueResource from 'vue-resource';
